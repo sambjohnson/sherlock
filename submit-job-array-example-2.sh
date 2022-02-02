317ae2b3f3abbdc711efde8660613822a3dd98ee
Sam Buck Johnson <92487911+sambjohnson@users.noreply.github.com>
2022-02-02 15:56:26 -0800

    Update submit-job-array-example-2.sh
    Edit parameters and add comments to make reuse easier.

--- submit-job-array-example-2.sh
+++ submit-job-array-example-2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-#SBATCH -J simple-array-example
-#SBATCH --time=00:0:30
+#SBATCH -J simple-array-example # template name; you should supply an informative one
+#SBATCH --time=00:0:30 # currently set to 30 seconds for testing purposes
 #SBATCH -n 1
-#SBATCH --array 1-10
+#SBATCH --array 1-10 # change upper limit (default: 10) to your number of subjects / number of jobs to make
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
-#SBATCH -p jyeatman,normal  # Queue names you can submit to
+#SBATCH -p jyeatman,normal,gpu  # queue names you can submit to
 # Outputs ----------------------------------
-#SBATCH -o %x-%A-%a.out
+#SBATCH -o %x-%A-%a.out # where to put logs and what to call them
 #SBATCH -e %x-%A-%a.err
-#SBATCH --mail-user=samjohns@stanford.edu
+# #SBATCH --mail-user=YOUR-EMAIL@stanford.edu # You should uncomment and replace with your own email
 #SBATCH --mail-type=END
 # ------------------------------------------
 
@@ -20,6 +20,6 @@ SUBJECTS_FILE='substest2.txt'
 subject=$( sed "${SLURM_ARRAY_TASK_ID}q;d" ${SUBJECTS_FILE} )
 
 # Compose the command line
-# python pytest.py $subject
-echo $subject
-echo $SLURM_ARRAY_TASK_ID
+python pytest.py $subject # test python
+echo $subject # test basic job array logic
+echo $SLURM_ARRAY_TASK_ID # test basic job array logic
